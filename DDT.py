73e8892984d557da9155432171bd51b7d5baa3e4
dpp291187 <160814414+dpp291187@users.noreply.github.com>
2025-04-07 12:27:25 +0900

    Update DDT.py

--- DDT.py
+++ DDT.py
@@ -3,10 +3,3 @@
 # n = number of input bits
 # m = number of output bits
 
-def ddt(S, n, m):
-    D = [[0] * (2 ** m) for _ in range(2 ** n)]
-    for alpha in range(2 ** n):
-        for x in range(2 ** n):
-            beta = S[x] ^ S[x ^ alpha]
-            D[alpha][beta] += 1
-    return D
